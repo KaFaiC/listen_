c6bacde3fa1990de8f885e90a161515fd37486b8
KaFaiC <choikafai2004@gmail.com>
2014-12-31 22:56:45 +0800

    add .env to .gitignore and add vote count to song

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,19 +3,22 @@ if( Songs.find().count() === 0) {
 	Songs.insert({
 		youtubeVideoId: 'ajCYQL8ouqw',
 		duration: 7*60,
-		title: 'Beatles - Let lt Be'
+		title: 'Beatles - Let lt Be',
+		voteCount: 6
 	});
 
 	Songs.insert({
 		youtubeVideoId: 'S09F5MejfBE',
 		duration: 3*60 + 55,
-		title: 'The Beatles - Yesterday '
+		title: 'The Beatles - Yesterday',
+		voteCount: 4
 	});
 
 	Songs.insert({
 		youtubeVideoId: 'DVg2EJvvlF8',
 		duration: 2*60 +33,
-		title: 'John Lennon - Imagine'
+		title: 'John Lennon - Imagine',
+		voteCount: -2
 	});
 
 }
